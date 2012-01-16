0fdbb2e87ab13657ae225a875e63faeae7f62aba
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2012-01-16 15:16:30 +0200

    Applied gzip transform to template handler by default

--- src/wheezy/web/handlers/template.py
+++ src/wheezy/web/handlers/template.py
@@ -1,7 +1,9 @@
 """
 """
 
+from wheezy.http.transforms import gzip_transform
 from wheezy.web.handlers.base import BaseHandler
+from wheezy.web.transforms import handler_transforms
 
 
 def template_handler(template_name, status_code=200):
@@ -18,6 +20,7 @@ class TemplateHandler(BaseHandler):
         self.status_code = status_code
         super(TemplateHandler, self).__init__(request)
 
+    @handler_transforms(gzip_transform)
     def get(self):
         response = self.render_response(self.template_name)
         response.status_code = self.status_code
