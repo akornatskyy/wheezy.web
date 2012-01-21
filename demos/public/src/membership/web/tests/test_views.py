1049a42d8e7beff6eada91b5cffdb80959721c14
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2012-01-21 13:01:09 +0200

    Added SignOutTestCase to public demo

--- demos/public/src/membership/web/tests/test_views.py
+++ demos/public/src/membership/web/tests/test_views.py
@@ -13,19 +13,45 @@ from config import options
 XSRF_NAME = options['XSRF_NAME']
 
 
+# region: pages
+
+class SignInPage(object):
+
+    def __init__(self, client):
+        assert '- Sign In</title>' in client.content
+        assert XSRF_NAME in client.cookies
+        self.client = client
+        self.form = client.form
+
+    def signin(self, username, password):
+        form = self.form
+        form.username = username
+        form.password = password
+        return self.client.submit(form)
+
+
+# region: mixins
+
 class SignInMixin(object):
 
     def signin(self, username, password):
         client = self.client
         assert 200 == client.get('/en/signin')
-        assert '- Sign In</title>' in client.content
-        assert XSRF_NAME in client.cookies
+        page = SignInPage(client)
+        return page.signin(username, password)
 
-        form = client.form
-        form.username = username
-        form.password = password
-        return client.submit(form)
 
+class SignOutMixin(object):
+
+    def signout(self):
+        client = self.client
+        assert 'Sign out</a>' in client.content
+        client.get('/en/signout')
+        assert 200 == client.follow()
+        assert 'Sign out</a>' not in client.content
+
+
+# region: test cases
 
 class SignInTestCase(unittest.TestCase, SignInMixin):
 
@@ -54,3 +80,20 @@ class SignInTestCase(unittest.TestCase, SignInMixin):
         self.signin('demo', 'P@ssw0rd')
         assert 200 == self.client.follow()
         assert 'Welcome <b>demo' in self.client.content
+
+
+class SignOutTestCase(unittest.TestCase, SignInMixin, SignOutMixin):
+
+    def setUp(self):
+        self.client = WSGIClient(main)
+
+    def tearDown(self):
+        del self.client
+        self.client = None
+
+    def test_signout_user(self):
+        """ Ensure sigin is successful.
+        """
+        self.signin('demo', 'P@ssw0rd')
+        assert 200 == self.client.follow()
+        self.signout()
