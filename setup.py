55f79aa659d320b91698fb973df9b5ab2a64dfac
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2012-02-18 14:21:22 +0200

    Updated list of setuptools classifiers

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
     license='MIT',
     classifiers=[
         'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
@@ -50,7 +51,17 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing',
         'Topic :: Utilities'
     ],
     keywords='wsgi web handler static file template mako errors routing '
@@ -64,7 +75,7 @@ setup(
     package_dir={'': 'src'},
     namespace_packages=['wheezy'],
 
-    zip_safe=True,
+    zip_safe=False,
     install_requires=install_requires,
     extras_require={
         'dev': [
