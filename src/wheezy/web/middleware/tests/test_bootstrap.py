ecc3f248113f6658f97466723a1196cb6e974aba
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2012-04-20 14:00:18 +0300

    Eliminated dependency on wheezy.http bootstrap options.

--- src/wheezy/web/middleware/tests/test_bootstrap.py
+++ src/wheezy/web/middleware/tests/test_bootstrap.py
@@ -14,20 +14,22 @@ class BootstrapWebDefaultsTestCase(unittest.TestCase):
     def test_default_options(self):
         """ Ensure required keys exist.
         """
+        from wheezy.http import bootstrap_http_defaults
         from wheezy.web.middleware.bootstrap import bootstrap_defaults
         options = {}
+        bootstrap_http_defaults(options)
+        http_required_options = set(options.keys())
 
         assert None == bootstrap_defaults({})(options)
 
-        required_options = tuple(sorted(options.keys()))
-        assert 21 == len(required_options)
+        required_options = tuple(sorted(
+            set(options.keys()) - http_required_options))
+        print(required_options)
+        assert 12 == len(required_options)
         assert ('AUTH_COOKIE', 'AUTH_COOKIE_DOMAIN', 'AUTH_COOKIE_PATH',
-                'AUTH_COOKIE_SECURE', 'CONTENT_TYPE', 'ENCODING',
-                'ENVIRON_HOST', 'ENVIRON_HTTPS', 'ENVIRON_HTTPS_VALUE',
-                'ENVIRON_REMOTE_ADDR', 'HTTP_COOKIE_DOMAIN',
-                'HTTP_COOKIE_HTTPONLY', 'HTTP_COOKIE_SECURE',
-                'MAX_CONTENT_LENGTH', 'RESUBMISSION_NAME', 'XSRF_NAME',
-                'path_for', 'path_router', 'render_template', 'ticket',
+                'AUTH_COOKIE_SECURE', 'CONTENT_TYPE',
+                'RESUBMISSION_NAME', 'XSRF_NAME', 'path_for', 'path_router',
+                'render_template', 'ticket',
                 'translations_manager') == required_options
 
     def test_path_router(self):
@@ -43,5 +45,4 @@ class BootstrapWebDefaultsTestCase(unittest.TestCase):
             'signin': 'signin'
         })(options)
 
-        required_options = tuple(sorted(options.keys()))
-        assert 21 == len(required_options)
+        assert tuple(options.keys())
