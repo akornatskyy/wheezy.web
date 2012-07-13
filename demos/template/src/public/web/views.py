f8bac61442e3b96935e67bf2214cabfbaae0cd04
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2012-07-13 13:49:11 +0300

    Specify translation name for public pages.

--- demos/template/src/public/web/views.py
+++ demos/template/src/public/web/views.py
@@ -3,11 +3,14 @@
 
 from wheezy.web.handlers import template_handler
 
+extra = {
+    'translation_name': 'public'
+}
 
-home = template_handler('public/home.html')
-about = template_handler('public/about.html')
+home = template_handler('public/home.html', **extra)
+about = template_handler('public/about.html', **extra)
 
-http400 = template_handler('public/http400.html', status_code=400)
-http403 = template_handler('public/http403.html', status_code=403)
-http404 = template_handler('public/http404.html', status_code=404)
-http500 = template_handler('public/http500.html', status_code=500)
+http400 = template_handler('public/http400.html', status_code=400, **extra)
+http403 = template_handler('public/http403.html', status_code=403, **extra)
+http404 = template_handler('public/http404.html', status_code=404, **extra)
+http500 = template_handler('public/http500.html', status_code=500, **extra)
--- demos/template/src/urls.py
+++ demos/template/src/urls.py
@@ -8,12 +8,12 @@ from wheezy.http.transforms import gzip_transform
 from wheezy.http.transforms import response_transforms
 from wheezy.routing import url
 from wheezy.web.handlers import file_handler
-from wheezy.web.handlers import template_handler
 
 from config import static_cache_profile
 from membership.web.urls import membership_urls
 from public.web.urls import error_urls
 from public.web.urls import public_urls
+from public.web.views import home
 
 
 locale_pattern = '{locale:(en|ru)}/'
@@ -27,10 +27,7 @@ static_files = response_cache(static_cache_profile)(
 locale_urls = public_urls + membership_urls
 locale_urls.append(('error/', error_urls, locale_defaults))
 all_urls = [
-    url('',
-        template_handler('public/home.html'),
-        locale_defaults,
-        name='default'),
+    url('', home, locale_defaults, name='default'),
     (locale_pattern, locale_urls, locale_defaults),
     url('static/{path:any}', static_files, name='static'),
     url('favicon.ico', static_files, {'path': 'img/favicon.ico'})
