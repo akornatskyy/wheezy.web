d2e5439e215d2fc5dffdf2cf3d57e3caf579249c
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2013-03-17 17:04:20 +0200

    Use handler_cache as a synonym for wheezy.http.response_cache.

--- src/wheezy/web/caching.py
+++ src/wheezy/web/caching.py
@@ -2,28 +2,7 @@
 """ ``caching`` module.
 """
 
+from wheezy.http import response_cache as handler_cache
 
-def handler_cache(profile):
-    """ Decorator that applies cache profile strategy to the
-        wrapping handler.
-    """
-    def decorate(method):
-        if not profile.enabled:
-            return method
 
-        if profile.request_vary:
-            def cache(handler, *args, **kwargs):
-                response = method(handler, *args, **kwargs)
-                response.cache_profile = profile
-                if response.cache_policy is None:
-                    response.cache_policy = profile.cache_policy()
-                return response
-            return cache
-        else:
-            def no_cache(handler, *args, **kwargs):
-                response = method(handler, *args, **kwargs)
-                if response.cache_policy is None:
-                    response.cache_policy = profile.cache_policy()
-                return response
-            return no_cache
-    return decorate
+assert handler_cache
