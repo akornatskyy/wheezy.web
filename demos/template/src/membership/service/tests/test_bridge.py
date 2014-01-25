7da087e52169f4622624d64bf8d58565edd1ef32
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2014-01-25 12:16:40 +0200

    Fixed service unit tests due to refactoring.

--- demos/template/src/membership/service/tests/test_bridge.py
+++ demos/template/src/membership/service/tests/test_bridge.py
@@ -12,67 +12,60 @@ from membership.repository.samples import next_registration
 class MembershipServiceTestCase(unittest.TestCase):
 
     def setUp(self):
-        from wheezy.core.i18n import TranslationsManager
         from membership.service.bridge import MembershipService
-        self.mock_repository = Mock()
+        self.mock_factory = Mock()
         self.service = MembershipService(
-            repository=self.mock_repository,
+            factory=self.mock_factory,
             errors={},
-            translations=TranslationsManager()['en'],
             locale='en')
 
 
 class CreateAccountTestCase(MembershipServiceTestCase):
 
-    def test_validation_errors(self):
-        """ Checks for validation errors.
-        """
-        from membership.models import Registration
-        r = Registration()
-        assert not self.service.create_account(r)
-        assert 7 == len(self.service.errors)
-
     def test_username_is_taken(self):
         """ If username is taken return an error.
         """
-        r = next_registration()
-        self.mock_repository.membership.has_account.return_value = True
+        r = next_registration(question_id='1')
+        f = self.mock_factory
+        f.membership.list_password_questions.return_value = (('1', ''),)
+        f.membership.has_account.return_value = True
 
         assert not self.service.create_account(r)
 
         assert 1 == len(self.service.errors)
-        assert '__ERROR__' in self.service.errors
-        self.mock_repository.membership.has_account.assert_called_once_with(
+        assert 'username' in self.service.errors
+        f.membership.has_account.assert_called_once_with(
             r.credential.username)
 
     def test_create_account_failed(self):
         """ If repository failed to create an account show a friendly
             message.
         """
-        r = next_registration()
-        self.mock_repository.membership.has_account.return_value = False
-        self.mock_repository.membership.create_account.return_value = False
+        r = next_registration(question_id='1')
+        f = self.mock_factory
+        f.membership.list_password_questions.return_value = (('1', ''),)
+        f.membership.has_account.return_value = True
+        f.membership.create_account.return_value = False
 
         assert not self.service.create_account(r)
 
         assert 1 == len(self.service.errors)
-        assert '__ERROR__' in self.service.errors
-        self.mock_repository.membership.has_account.assert_called_once_with(
+        assert 'username' in self.service.errors
+        f.membership.has_account.assert_called_once_with(
             r.credential.username)
-        self.mock_repository.membership.create_account.assert_called_once_with(
-            r)
 
     def test_succeed(self):
         """ The account has been created successfully.
         """
-        r = next_registration()
-        self.mock_repository.membership.has_account.return_value = False
-        self.mock_repository.membership.create_account.return_value = True
+        r = next_registration(question_id='1')
+        f = self.mock_factory
+        f.membership.list_password_questions.return_value = (('1', ''),)
+        f.membership.has_account.return_value = False
+        f.membership.create_account.return_value = True
 
         assert self.service.create_account(r)
 
         assert not self.service.errors
-        self.mock_repository.membership.has_account.assert_called_once_with(
+        f.membership.has_account.assert_called_once_with(
             r.credential.username)
-        self.mock_repository.membership.create_account.assert_called_once_with(
-            r)
+        f.membership.create_account.assert_called_once_with(r)
