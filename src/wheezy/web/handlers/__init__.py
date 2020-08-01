aed8caee6fe094dd09ef6134853db349fcbf9b31
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2020-08-01 20:46:32 +0300

    Updated module exports.

--- src/wheezy/web/handlers/__init__.py
+++ src/wheezy/web/handlers/__init__.py
@@ -13,3 +13,15 @@ from wheezy.web.handlers.file import file_handler
 from wheezy.web.handlers.method import MethodHandler
 from wheezy.web.handlers.template import TemplateHandler
 from wheezy.web.handlers.template import template_handler
+
+__all__ = (
+    "BaseHandler",
+    "RedirectRouteHandler",
+    "permanent_redirect_handler",
+    "redirect_handler",
+    "FileHandler",
+    "file_handler",
+    "MethodHandler",
+    "TemplateHandler",
+    "template_handler",
+)
--- src/wheezy/web/middleware/__init__.py
+++ src/wheezy/web/middleware/__init__.py
@@ -7,3 +7,9 @@
 from wheezy.web.middleware.bootstrap import bootstrap_defaults
 from wheezy.web.middleware.errors import http_error_middleware_factory
 from wheezy.web.middleware.routing import path_routing_middleware_factory
+
+__all__ = (
+    "bootstrap_defaults",
+    "http_error_middleware_factory",
+    "path_routing_middleware_factory",
+)
